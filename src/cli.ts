df96933644cce1999d19bbc045b3f885f0ae4586
ujnss <unlinunlin@163.com>
2025-12-11 08:15:03 +0000

    feat: add cli for prover client

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,65 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { ProverClient } from "./prover_client.js";
+import fs from "fs";
 
 async function main() {
+  const api = new ProverClient();
   const program = new Command();
+
+  program
+    .command("uploadProgram")
+    .description("uploadProgram.")
+    .requiredOption("--filepath <FILEPATH>", "Program file path")
+    .option("--name <NAME>", "Program Name", "Default Name")
+    .option("--version <VERSION>", "Version number", "1.0")
+    .option("--description <DESCRIPTION>", "Description", "Default description.")
+    .action(async (opts) => {
+      console.log("uploadProgram");
+      const result = await api.uploadProgram(opts.filepath, 'My Program', '1.0', 'Program description');
+      console.log('Program uploaded, ID:', result.program_id);
+    });
+
+  program
+    .command("submitTask")
+    .description("submitTask.")
+    .requiredOption("--filepath <FILEPATH>", "Attestation data file path")
+    .option("--programId <PROGRAM_ID>", "Program id")
+    .action(async (opts) => {
+      console.log("submitTask");
+      const attestation_data = fs.readFileSync(opts.filepath, { encoding: "utf-8" });
+      const result = await api.submitTask(attestation_data, opts.programId);
+      console.log('Task submitted:', result);
+    });
+
+  program
+    .command("getResult")
+    .description("getResult.")
+    .requiredOption("--taskId <TASK_ID>", "Task id")
+    .action(async (opts) => {
+      console.log("getResult");
+      const result = await api.getResult(opts.taskId);
+      console.log('getResult:', result);
+    });
+
+  program
+    .command("listTasks")
+    .description("listTasks.")
+    .action(async (_opts) => {
+      console.log("listTasks");
+      const result = await api.listTasks();
+      console.log('listTasks:', result);
+    });
+
+  program
+    .command("listPrograms")
+    .description("listPrograms.")
+    .action(async (_opts) => {
+      console.log("listPrograms");
+      const result = await api.listPrograms();
+      console.log('listPrograms:', result);
+    });
 
   program.parseAsync();
 }
