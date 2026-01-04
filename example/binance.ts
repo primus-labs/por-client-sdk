f510168dc69cfd868ff17e1273d810434d72c817
ujnss <unlinunlin@163.com>
2026-01-04 02:53:06 +0000

    update example

--- example/binance.ts
+++ example/binance.ts
@@ -1,15 +1,22 @@
-import { DataSource, PoRClient } from "../src";
+import { DataSource, PoRClient, loadConfigFromFile } from "../src";
 
 async function main() {
   try {
-    const ds = new DataSource.Binance();
-    const requestParams1 = ds.getUnifiedAccountRequests();
-    const requestParams2 = ds.getSpotAccountRequests();
+    const config = loadConfigFromFile();
+    console.log("config:", JSON.stringify(config));
+    const ds = new DataSource.ExchangeManager(config.exchanges);
+    console.log("ds:", JSON.stringify(ds));
 
-    const client = new PoRClient();
-    const result = await client.run([requestParams1, requestParams2]);
-    console.log("result", JSON.stringify(result));
-    console.log('proof fixture(json):', JSON.parse(result?.proof_fixture ?? "{}"));
+    const client = new PoRClient(config.app);
+    console.log("client:", JSON.stringify(client));
+
+    if (ds.binance?.hasSpot) {
+      const requestParams = ds.binance?.getSpotAccountInfoRequests();
+      const result = await client.run(requestParams);
+      console.log("result", JSON.stringify(result));
+      console.log('proof fixture(json):', JSON.parse(result?.proof_fixture ?? "{}"));
+    }
+
   } catch (err: any) {
     console.log("err:", err?.message, JSON.stringify(err));
   }
